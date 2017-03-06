946992e37cb09ea3036b6659f3b574edc4259bb5
BraulioV <brauliovarlop@correo.ugr.es>
2017-03-06 18:20:01 +0100

    method to read classrooms from csv

--- src/classroom.py
+++ src/classroom.py
@@ -1,11 +1,27 @@
 import numpy as np
 
 
+def create_classroom(filename, days, hours_per_day):
+    # Empty list of different classrooms
+    classroom = []
+    with open(filename, 'r') as f:
+        # Skip the name of the columns
+        f.readline()
+        # Load data
+        for line in f:
+            l = line[:-1].split(',')
+            classroom.append(ClassRoom(days, hours_per_day, l[0], int(l[1])))
+
+    return classroom
+
+
 class ClassRoom:
 
-    """" Class to model a classroom for a specific bachelor"""
+    """" Class to model a classroom for a specific bachelor """
 
-    def __init__(self, d, x, capacity, name):
+    def __init__(self, d, x, name, capacity):
         self.time_table = np.full((d, x), fill_value=False, dtype=np.bool)
         self.capacity = capacity
         self.classroom_name = name
+
+
