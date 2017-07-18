bb2543722e0713dea89cb713d3199c1f46d4e043
mgmacias95 <mgmacias95@correo.ugr.es>
2017-07-18 20:01:07 +0200

    Merge branch 'master' of /home/marta/Documentos/TFG/TFG with conflicts.

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ from subject import *
 from sub_group import *
 from practice_classroom import *
 from timetable import *
-from exams import *
 
 HOURS = 12
 DAYS = 5
@@ -16,23 +15,20 @@ if __name__ == '__main__':
     groups = create_group(filename="../Dataset/groups.csv", 
                           classroom_list=classrooms)
     # print(groups)
-    subjects, n_years = create_subject(filename="../Dataset/subjects.csv")
+    subjects = create_subject(filename="../Dataset/subjects.csv")
     subjects1 = dict(filter(lambda s: s[1].semester == 1, subjects.items()))
     # print(subjects)
     pclassrooms = create_practice_classroom(filename="../Dataset/practiceclassrooms.csv", days=DAYS, hours_per_day=HOURS)
     # print(pclassrooms)
 
     # create a timetable object
-    # timetable = TimeTable(n_days=DAYS, n_hours=HOURS, groups=groups,
-    #                       classrooms=classrooms, practices_classrooms=pclassrooms,
-    #                       subjects=subjects1, semester=1)
+    timetable = TimeTable(n_days=DAYS, n_hours=HOURS, groups=groups,
+                          classrooms=classrooms, practices_classrooms=pclassrooms,
+                          subjects=subjects1, semester=1)
 
     # timetable.__get_possible_classrooms__()
 
-    exams = Exams(n_days = 10, groups = groups, classrooms = classrooms,
-                  subjects = subjects1, semester = 1, years = n_years)
-
-    print(exams.time_table)
-    # timetable.random_greedy_theory(1)
-    #timetable.random_greedy_practice(1)
-    # print(timetable.time_table)
+    timetable.asign_hours(1)
+    #timetable.random_greedy_theory(1)
+    # timetable.random_greedy_practice(1)
+    #print(timetable.time_table)
