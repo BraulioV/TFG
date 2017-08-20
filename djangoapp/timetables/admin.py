078c5fdcf68ec2128f8ad854793b97c35fa92c82
Marta Gómez <mgmacias95@correo.ugr.es>
2017-08-20 14:25:10 +0200

    script working

--- djangoapp/timetables/admin.py
+++ djangoapp/timetables/admin.py
@@ -4,4 +4,3 @@ from .models import *
 admin.site.register(Subject)
 admin.site.register(Group)
 admin.site.register(Classroom)
-admin.site.register(PracticeClassroom)
--- djangoapp/timetables/models.py
+++ djangoapp/timetables/models.py
@@ -1,33 +1,26 @@
 from django.db import models
 
-class PracticeClassroom(models.Model):
-    name     = models.CharField(max_length=50)
-    capacity = models.PositiveSmallIntegerField(default=0)
-    
-    def __str__(self):
-        return "{}, {}".format(self.name, self.capacity)
-
-class Subject(models.Model):
-    name              = models.CharField(max_length=200)
-    thours            = models.PositiveSmallIntegerField(default=0)
-    phours            = models.PositiveSmallIntegerField(default=0)
-    acronym           = models.CharField(max_length=10)
-    speciality        = models.CharField(max_length=200)
-    year              = models.PositiveSmallIntegerField(default=0)
-    semester          = models.PositiveSmallIntegerField(default=0)
-    degree            = models.CharField(max_length=200)
-    students          = models.PositiveSmallIntegerField(default=0)
-    practiceclassroom = models.ManyToManyField(PracticeClassroom)
-
-    def __str__(self):
-        return "{}, {}".format(self.name, self.acronym)
-
 class Classroom(models.Model):
     name     = models.CharField(max_length=50)
     capacity = models.PositiveSmallIntegerField(default=0)
 
     def __str__(self):
         return "{}, {}".format(self.name, self.capacity)
+
+class Subject(models.Model):
+    name       = models.CharField(max_length=200)
+    thours     = models.PositiveSmallIntegerField(default=0)
+    phours     = models.PositiveSmallIntegerField(default=0)
+    acronym    = models.CharField(max_length=10)
+    speciality = models.CharField(max_length=200)
+    year       = models.PositiveSmallIntegerField(default=0)
+    semester   = models.PositiveSmallIntegerField(default=0)
+    degree     = models.CharField(max_length=200)
+    students   = models.PositiveSmallIntegerField(default=0)
+    classroom  = models.ManyToManyField(Classroom)
+
+    def __str__(self):
+        return "{}, {}".format(self.name, self.acronym)
 
 class Group(models.Model):
     name          = models.CharField(max_length=50)
