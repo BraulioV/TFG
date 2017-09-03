b387b98022a7b45ae070fb437c49d3781e668c4f
Marta Gómez <marta@wazuh.com>
2017-09-03 15:56:40 +0200

    select group

--- djangoapp/timetables/views.py
+++ djangoapp/timetables/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .timetable_main import *
+from .models import Groups
 
 def index(request):
     return render(request, 'timetables/index.html')
@@ -13,10 +14,13 @@ def senddata(request):
 
     request.session['days']  = days
     request.session['hours'] = hours
-    request.session['timetable'] = timetable.default()
+    request.session['full_timetable'] = timetable.default()
 
     return HttpResponseRedirect(reverse('timetables:showdata', args=('1A',)))
 
 
 def showdata(request, groupname):
+    request.session['timetable']    = request.session['full_timetable'][groupname]
+    request.session['numsubgroups'] = Groups.objects.get(name=groupname).numsubgroups
+
     return render(request, 'timetables/showdata.html', context={'groupname':groupname})
